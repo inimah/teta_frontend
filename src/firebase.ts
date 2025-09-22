632f6ec6cb3f87b7407a1206fddf03808b4ef797
Andri Fachrur Rozie <4r53n1c@gmail.com>
2025-09-22 20:04:31 +0700

    Configurable firebase auth keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBti79WCpkH3RNZWxkiC8zR3aUFf2wdfso",
-  authDomain: "auth-1fa0a.firebaseapp.com",
-  projectId: "auth-1fa0a",
-  storageBucket: "auth-1fa0a.firebasestorage.app",
-  messagingSenderId: "470951034832",
-  appId: "1:470951034832:web:fdace6a532df4b33bb83d5",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
